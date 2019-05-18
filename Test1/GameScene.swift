6291d78f97ed571de5868bec4f96ca30e71e54d8
Steven Gunawan <stevengunawan1512@gmail.com>
2019-05-18 19:42:27 +0700

    bisa jalan

--- Test1/GameScene.swift
+++ Test1/GameScene.swift
@@ -11,79 +11,113 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var panda = SKSpriteNode()
+    var turtle = SKSpriteNode()
+    
+//    let tapRec = UITapGestureRecognizer()
     
     override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        setUp()
+//        turtle = self.childNode(withName: "Turtle") as! SKSpriteNode
+
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//                Timer.scheduledTimer(timeInterval: TimeInterval(1), target: self, selector: #selector(GameScene.createRoadStripLeft), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: TimeInterval(0.4), target: self, selector: #selector(GameScene.createRoadStripRight), userInfo: nil, repeats: true)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+//        tapRec.addTarget(self, action: #selector(GameScene.tappedView(_:)))
+//        tapRec.numberOfTouchesRequired = 1
+//        tapRec.numberOfTapsRequired = 1
+//        self.view!.addGestureRecognizer(tapRec)
+        
+//        let gesture = UITapGestureRecognizer(target: self, action: #selector(move))
+//        self.turtle.gesture
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+//    }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    @objc func move(){
+        
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        showRoadStrip()
     }
+    func setUp() {
+        turtle = self.childNode(withName: "Turtle") as! SKSpriteNode
+        panda = self.childNode(withName: "Panda") as! SKSpriteNode
+    }
+    @objc func createRoadStripLeft() {
+        let leftRoadStrip = SKShapeNode(rectOf: CGSize(width: 30, height: 300))
+        leftRoadStrip.strokeColor = SKColor.white
+        leftRoadStrip.fillColor = SKColor.white
+        leftRoadStrip.alpha = 0.4
+        leftRoadStrip.name = "leftRoadStrip"
+        leftRoadStrip.zPosition = 10
+        leftRoadStrip.position.x = -187.5
+        leftRoadStrip.position.y = 700
+        addChild(leftRoadStrip)
+    }
+    
+    @objc func createRoadStripRight(){
+        let rightRoadStrip = SKShapeNode(rectOf: CGSize(width: 30, height: 100))
+        rightRoadStrip.strokeColor = SKColor.white
+        rightRoadStrip.fillColor = SKColor.white
+        rightRoadStrip.alpha = 0.4
+        rightRoadStrip.name = "rightRoadStrip"
+        rightRoadStrip.zPosition = 10
+        rightRoadStrip.position.x = 187.5
+        rightRoadStrip.position.y = 700
+        addChild(rightRoadStrip)
+    }
+    
+    func showRoadStrip(){
+        enumerateChildNodes(withName: "leftRoadStrip", using: { (roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        })
+        enumerateChildNodes(withName: "rightRoadStrip", using: { (roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        })
+    }
+    
+//    func touchDown(atPoint pos: CGPoint) {
+//        walk()
+//    }
+//
+//    func walk(){
+//       turtle.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
+//    }
+    
+//    func tutleTapped(){
+//    }
+   
+    
+//    func jump() {
+//        turtle.texture = SKTexture(imageNamed: "Turtle_jumping")
+//        turtle.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
+//    }
+    
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//
+//    func touchUp(atPoint pos: CGPoint) {
+//        turtle.texture = SKTexture(imageNamed: "turtle_standing")
+//    }
+  
+//    @objc func tappedView(_ sender:UITapGestureRecognizer){
+//        let point:CGPoint = sender.location(in: self.view)
+//        print("Single Tap")
+//        print(point)
+//    }
+    
+    
+
 }
